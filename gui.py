1639e6ea7f00a4276113436a848349befa121e0d
Jmint <jeffreymintah737@gmail.com>
2024-03-27 09:38:05 +0000

    gui: changes

--- gui.py
+++ gui.py
@@ -163,17 +163,20 @@ class MainWindow(QMainWindow):
 
     def show_modern_page(self):
         self.central_widget.setCurrentIndex(1)
+        
     def show_home_page(self):
-        layout = self.central_widget.layout()
-        for i in reversed(range(layout.count())):
-            widget = layout.itemAt(i).widget()
-            if widget:
-                widget.setParent(None)
+    # Clear existing widgets in the layout
+    layout = self.central_widget.layout()
+    for i in reversed(range(layout.count())):
+        widget = layout.itemAt(i).widget()
+        if widget:
+            widget.setParent(None)
 
-        # Add a label to show that we are on the home page
-        home_label = QLabel("Welcome to the Home Page!", self)
-        home_label.setStyleSheet("font-size: 24px; color: white;")
-        layout.addWidget(home_label, alignment=Qt.AlignmentFlag.AlignCenter)
+    # Add a label to show that we are on the home page
+    home_label = QLabel("Welcome to the Home Page!", self)
+    home_label.setStyleSheet("font-size: 24px; color: white;")
+    layout.addWidget(home_label)
+    layout.setAlignment(home_label, Qt.AlignmentFlag.AlignCenter)
     
     def show_main_window(self):
         self.central_widget.setCurrentIndex(0)
