242a7ce63d3e8ebc8b13dcce20a3899052e9d868
Jmint <jeffreymintah737@gmail.com>
2024-03-30 12:41:33 +0000

    spacing\

--- gui.py
+++ gui.py
@@ -462,6 +462,7 @@ class MainWindow(QMainWindow):
         current_widget = self.central_widget.currentWidget()
         if isinstance(current_widget, ExtractPatternPage):
             current_widget.result_label.setText(f"Extraction Error: {error_message}")
+            
 if __name__ == "__main__":
     app = QApplication([])
     window = MainWindow()
